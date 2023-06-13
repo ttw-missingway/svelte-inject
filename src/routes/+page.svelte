6691e2e8c1de08e6a2f5378c7c30b19e1d2eae71
Missingway <ttw.missingway@gmail.com>
2023-06-13 14:22:57 -0500

    svelte-in-webflow

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    let count = 0;
+
+    const increment = () => {
+        count++;
+    }
+
+    const decrement = () => {
+        count--;
+    }
+</script>
+
+<h1>
+    {count}
+</h1>
+
+<button on:click={increment}>
+    Increment
+</button>
+
+<button on:click={decrement}>
+    Decrement
+</button>
